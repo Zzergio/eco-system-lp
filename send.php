131990c3c75ab010d9e8fdb91265deca7710fbd9
Zzergio <design@cursor.net.ua>
2018-12-21 18:54:13 +0200

    Script is ready

--- send.php
+++ send.php
@@ -1,24 +1,24 @@
 <?php
 
 $name=$_POST['user-name'];
-$phone=$_POST['user-phone']
+$phone=$_POST['user-phone'];
 $email=$_POST['user-email'];
 $to = "s.rieznyk@gmail.com";
 
-$subject = "From Eco System LP";
+$subject = "Заявка Eco System LP";
 
 $message = "
-
+<p>Пришла заявка с сайта</p>
 <strong>Name:</strong> ".htmlspecialchars($name)."<br /><br />
-<strong>Email:</strong> <a href='mailto:$email'>".htmlspecialchars($email)."</a> <br /><br />
-<strong>Phone:</strong> ".htmlspecialchars($phone).;
+<strong>Email:</strong> ".htmlspecialchars($email)." <br /><br />
+<strong>Phone:</strong> ".htmlspecialchars($phone);
 
 // Отправляем письмо при помощи функции mail();
-$headers = "From: Eco System\r\nContent-type: text/html; charset=UTF-8 \r\n";
+
+$headers = "Content-type: text/html; charset=UTF-8 \r\n";
 mail ($to, $subject, $message, $headers);
 
-// Перенаправляем человека на страницу благодарности и завершаем скрипт
-header('Location: thanks/');
+header('Location: thanks.html');
 exit();
 
 ?>
